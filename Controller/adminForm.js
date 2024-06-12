552a7e4ffad7d5ea4c6f29535332258896ec6c7f
sa7ik <mohammedsabikoo7@gmail.com>
2024-06-12 09:52:12 +0530

    updated

--- Controller/adminForm.js
+++ Controller/adminForm.js
@@ -2,6 +2,7 @@ const productSchema=require("../Model/product")
 const joi=require("joi")
 const UserSchema=require("../Model/UserSchema")
 const cartSchema=require("../Model/CartSchema")
+const OrderSchema=require("../Model/OrderSchema")
 
 // joi validation
 
@@ -97,4 +98,121 @@ const viewProductById=async (req,res)=>{
     else{
         res.status(200).json(product)
     }
+}
+
+// add product
+
+const addProduct=async(req,res)=>{
+    const {token}=req.cookies
+    const data=req.body
+    data.image=req.cloudinaryImageUrl
+
+    const {name,description,price,image}=data;
+
+    const validate = await Schema.validate(data)
+    if(!validate){
+        return res.status(400).send("product not validated")
+    }
+    const existingProduct=await productSchema.findOne({name:name})
+    if (existingProduct){
+        return res.status(400).json({
+            success:false,
+            message:"product with same name already exists",
+        })
+    }
+    const newProduct=new productSchema({
+        name,
+        description,
+        price,
+        image,
+    })
+    await newProduct.save();
+    res.status(200).send("Product added successfully")
+}
+
+// update product
+
+const updateProduct=async (req,res)=>{
+    const {token}=req.cookies
+    const {id}=req.params
+
+    const data=req.body;
+
+    const {name,description,price,image}=data
+    const {error}=Schema.validate(data);
+    if (error){
+        res.status(400).send({message:error.details[0].message})
+    }
+    const product=await productSchema.findById(id);
+
+    if (!product){
+        return res.status(404).json({success:false,message:"Product not found"})
+    }
+    product.image = req.cloudinaryImageUrl
+
+    await productSchema.findByIdAndUpdate(
+        {_id:id},
+        {
+            name:name,
+            image:req.cloudinaryImageUrl,
+            price:price,
+            description:description,
+        }
+    )
+    res.status(201).json({
+        success:true,
+        message:"Product updated"
+    })
+}
+
+// delete product
+
+const deleteProduct=async(req,res)=>{
+    const {token}=req.cookies
+    const {id}=req.params
+
+    const deleted=await productSchema.findByIdAndDelete(id)
+    if(!deleted){
+        return res.status(404).json({
+            success:false,
+            message:"Product not found",
+        })
+    }
+    res.status(200).json({
+        success:true,
+        message:"product deleted successfully"
+    })
+}
+
+// order details
+
+const orders = async (req,res)=>{
+    const {token}=req.cookies
+
+    const products=await OrderSchema.find().populate("products.productId")
+
+    if (products.length===0){
+        res.status(404).json({
+            success:false,
+            messages:"No orders found"
+        })
+    }
+    res.status(200).json(products)
+}
+
+//separate order
+
+const ordersById=async (req,res)=>{
+    const {token}=req.cookies
+    const {id}=req.params
+
+    const products=await OrderSchema.findOne({userId:userId}).populate("products.productId")
+
+    if (products.length===0){
+        res.status(404).json({
+            success:false,
+            message:"No orders found"
+        })
+    }
+    res.status(200).json(products)
 }
---
+++ Model/OrderSchema.js
@@ -0,0 +1,40 @@
+const mongoose=require("mongoose")
+
+const orderSchema = new mongoose.Schema({
+    products:[
+        {
+            productId:{
+                type:mongoose.Schema.Types.ObjectId,
+                ref:"product",
+                required:true,
+            },
+            quantity:{
+                type:Number,
+                default:1,
+            }
+        }
+    ],
+    userId:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"UserSchema",
+        required:true
+    },
+    purchaseDate:{
+        type:Date,
+        default:Date.now,
+        immutable:true,
+    },
+    totalItems:{
+        type:Number,
+        default:0
+    },
+    totalPrice:{
+        type:Number,
+        default:0
+    },
+    orderId:{
+        type:String
+    },
+});
+
+module.exports=mongoose.model("Order",orderSchema)
