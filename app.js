0a055fe6f65408e7cbbe6ca1ae71ad4c109a2f45
sa7ik <mohammedsabikoo7@gmail.com>
2024-06-27 14:40:04 +0530

    updated

--- app.js
+++ app.js
@@ -2,13 +2,15 @@ require('dotenv').config()
 const express=require('express')
 const app=express()
 const mongoose=require('mongoose')
-const port=3000
+const port=3010
+const cors=require("cors")
 const userRoute=require('./Routes/userRoute')
 const adminRoute=require('./Routes/adminRoute')
 const cookieParser=require('cookie-parser')
 
 mongoose.connect("mongodb://localhost:27017/BabyProducts").then(res=>console.log("connected dbs")).catch(err=>console.log(err))
 
+app.use(cors({origin:"http://localhost:3000",credentials:true}))
 app.use(express.json())
 app.use(cookieParser())
 app.use('/api',userRoute);
